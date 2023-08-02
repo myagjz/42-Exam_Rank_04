62eea393b4b52f0e9ce6991596638d94ba235f36
Mustafa YAĞIZ <112881823+myagjz@users.noreply.github.com>
2023-08-02 15:24:04 +0300

    Update microshell.c

--- microshell.c
+++ microshell.c
@@ -1,64 +1,64 @@
 #include "microshell.h"
 
-int err(char *str) 
+int	err(char *str)
 {
-    while (*str)
-        write(2, str++, 1);
-    return 1;
+	while (*str)
+		write (2, str++, 1);
+	return 1;
 }
 
-int cd(char **argv, int i) 
+int	cd(char **av, int i)
 {
-    if (i != 2)
-        return err("error: cd: bad arguments\n");
-    else if (chdir(argv[1]) == -1)
-        return err("error: cd: cannot change directory to "), err(argv[1]), err("\n");
-    return 0;
+	if (i != 2)
+		return err("error: cd: bad arguments\n");
+	else if (chdir(av[1]) == -1)
+		return err("error: cd: canoot change directory to "), err(av[1]), err("\n");
+	return 0;
 }
 
-int exec(char **argv, char **envp, int i) 
+int	exec(char **av, char **envp, int i)
 {
-    int fd[2];
-    int status;
-    int has_pipe = argv[i] && !strcmp(argv[i], "|");
+	int fd[2];
+	int status;
+	int has_pipe = av[i], !strcmp(av[i], "|");
 
-    if (has_pipe && pipe(fd) == -1)
-        return err("error: fatal\n");
+	if (has_pipe && pipe(fd) == -1)
+		return err("error: fatal\n");
 
-    int pid = fork();
-    if (!pid) 
-    {
-        argv[i] = 0;
-        if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-            return err("error: fatal\n");
-        execve(*argv, argv, envp);
-        return err("error: cannot execute "), err(*argv), err("\n");
-    }
+	int	pid = fork();
+	if (!pid)
+	{
+		av[i] = 0;
+		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+			return err("error: fatal\n");
+		execve(*av, av, envp);
+		return err("error: cannot execute "), err(*av), err("\n");
+	}
 
-    waitpid(pid, &status, 0);
-    if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-        return err("error: fatal\n");
-    return WEXITSTATUS(status);
+	watpid(pid, &status, 0);
+	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+			return err("error: fatal\n");
+	return WEXITSTATUS(status);
 }
 
-int main(int argc, char **argv, char **envp) 
+int	main(int ac, char **av, char **envp)
 {
-    int    i = 0;
-    int    status = 0;
+	int i = 0;
+	int status = 0;
 
-    if (argc > 1) 
-    {
-        while (argv[i] && argv[++i]) 
-        {
-            argv += i;
-            i = 0;
-            while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
-                i++;
-            if (!strcmp(*argv, "cd"))
-                status = cd(argv, i);
-            else if (i)
-                status = exec(argv, envp, i);
-        }
-    }
-    return status;
+	if (ac > 1)
+	{
+		while (av[i] && av[++i])
+		{
+			av += i;
+			i = 0;
+			while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+				i++;
+			if (!strcmp(*av, "cd"))
+				status = cd(av, i);
+			else if (i)
+				status = exec(av, envp, i);
+		}
+	}
+	return status;
 }
